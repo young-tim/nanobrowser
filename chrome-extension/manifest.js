dadb6060a8a1428977c7636f8c45850f4813398d
Tim <quyangtian@126.com>
2025-12-15 00:43:58 +0800

    变更项目名称

--- chrome-extension/manifest.js
+++ chrome-extension/manifest.js
@@ -39,7 +39,7 @@ function withOperaSidebar(manifest) {
   return deepmerge(manifest, {
     sidebar_action: {
       default_panel: 'side-panel/index.html',
-      default_title: 'Nanobrowser',
+      default_title: 'Aibrowser',
       default_icon: 'icon-32.png',
     },
   });
